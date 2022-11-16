84f910bcf15bdae4ccad04989df75d703040a118
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2022-11-16 16:18:23 -0300

    Configurando o AppError no arquivo server.ts

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,13 +1,30 @@
 import "reflect-metadata";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import routes from "./routes";
+import AppError from "../errors/AppError";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      response.status(error.statusCode).json({
+        status: "error",
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: "Internal server orro",
+    });
+  }
+);
 
 app.listen(3001, () => {
   console.log("Server started on port 3001!");
