be41edd2a94ed8ac8cf3064d2b21b8205b601f35
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2022-11-18 20:51:27 -0300

    Complemento do commit anterior para que seja possível acessar as rotas das tasks

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,5 +1,8 @@
+import tasksRouter from "@modules/Tasks/routes/tasks.routes";
 import { Router } from "express";
 
 const routes = Router();
+
+routes.use("/tasks", tasksRouter);
 
 export default routes;
