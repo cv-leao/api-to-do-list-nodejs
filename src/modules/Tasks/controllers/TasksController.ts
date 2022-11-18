15a0ab76a7474d64ec4e4d73cdb8f134113e011a
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2022-11-18 20:37:44 -0300

    Criação do Controller das Tasks

---
+++ src/modules/Tasks/controllers/TasksController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import CreateTaskService from "../services/CreateTaskService";
+
+class TasksController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { description, status } = await request.body;
+
+    const createTaskService = new CreateTaskService();
+
+    const task = await createTaskService
+      .execute({ description, status })
+      .catch((error) => {
+        response.statusCode;
+        return error;
+      });
+
+    return response.json(task);
+  }
+}
