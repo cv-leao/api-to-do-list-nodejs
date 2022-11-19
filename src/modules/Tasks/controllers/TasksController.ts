d12eaa950c1ad29f7e14c7997225c68aee3011ce
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2022-11-19 01:55:17 -0300

    Adicionando no controller o serviço de listar todas as tasks

--- src/modules/Tasks/controllers/TasksController.ts
+++ src/modules/Tasks/controllers/TasksController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import CreateTaskService from "../services/CreateTaskService";
+import ListTasksService from "../services/ListTasksService";
 
 class TasksController {
   public async create(request: Request, response: Response): Promise<Response> {
@@ -15,6 +16,20 @@ class TasksController {
       });
 
     return response.json(task);
+  }
+
+  public async indexAll(
+    request: Request,
+    response: Response
+  ): Promise<Response> {
+    const listTasksService = new ListTasksService();
+
+    const tasks = listTasksService.execute().catch((error) => {
+      response.statusCode;
+      return error;
+    });
+
+    return response.json(tasks);
   }
 }
 
