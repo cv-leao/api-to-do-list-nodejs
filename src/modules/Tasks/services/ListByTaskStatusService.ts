b660f209b0e54b8965459d165c197d8f3bf43826
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2022-11-23 16:17:31 -0300

    Criação do service de listar as tasks com base no status

---
+++ src/modules/Tasks/services/ListByTaskStatusService.ts
@@ -0,0 +1,29 @@
+import { prismaClient } from "src/database/prismaClient";
+import AppError from "@shared/errors/AppError";
+import { Tasks } from "@prisma/client";
+
+interface IListByStatus {
+  statusName: string;
+}
+
+class ListByTaskStatusService {
+  public async execute({ statusName }: IListByStatus): Promise<Tasks[]> {
+    if (
+      statusName != "A Fazer" &&
+      statusName != "Fazendo" &&
+      statusName != "feito"
+    ) {
+      throw new AppError("Status inexistente.");
+    }
+
+    const tasks = await prismaClient.tasks.findMany({
+      where: {
+        status: statusName,
+      },
+    });
+
+    return tasks;
+  }
+}
+
+export default ListByTaskStatusService;
